08282ed4f1a0ee730cfa2f74b898a24bf7dc760b
aadityamahindru <aaditya2069@gmail.com>
2020-04-10 00:41:48 +0530

    new commit

---
+++ Dp/tiling.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(tiles(n));
+
+    }
+    public static int tiles(int n){
+        int dp[]=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
