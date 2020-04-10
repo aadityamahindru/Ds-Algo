e9b2c75f9b0d8a32cd5b3971f5bdc1b56077cea3
aadityamahindru <aaditya2069@gmail.com>
2020-04-11 03:14:08 +0530

    new commit

---
+++ Recursion/printmaze.java
@@ -0,0 +1,34 @@
+import java.io.*;
+	import java.util.*;
+
+	public class Main {
+
+	    public static void main(String[] args) throws Exception {
+	        Scanner sc=new Scanner(System.in);
+	        int n=sc.nextInt();
+	        int m=sc.nextInt();
+	        printMazePaths(0,0,n-1,m-1,"");
+	        System.out.println();
+
+	    }
+
+	    // sr - source row
+	    // sc - source column
+	    // dr - destination row
+	    // dc - destination column
+	    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+	        if(sr==dr&&sc==dc)
+	        {
+	            System.out.println(psf);
+	            return;
+	        }
+	        if(sc>dc||sr>dr)
+	        {
+	            return;
+	        }
+	        printMazePaths(sr,sc+1,dr,dc,psf+"h");
+	        printMazePaths(sr+1,sc,dr,dc,psf+"v");
+	        
+	    }
+
+	}
