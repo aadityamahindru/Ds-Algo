5ff404f302980a4e2f7ef284a895eb1912f59099
aadityamahindru <aaditya2069@gmail.com>
2020-06-24 14:59:49 +0530

    hashmap implementation

---
+++ HashMap And Heap/hashMapImplementation.java
@@ -0,0 +1,174 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class HashMap<K, V> {
+    private class HMNode {
+      K key;
+      V value;
+
+      HMNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+      }
+    }
+
+    private int size; // n
+    private LinkedList<HMNode>[] buckets; // N = buckets.length
+
+    public HashMap() {
+      initbuckets(4);
+      size = 0;
+    }
+
+    private void initbuckets(int N) {
+      buckets = new LinkedList[N];
+      for (int bi = 0; bi < buckets.length; bi++) {
+        buckets[bi] = new LinkedList<>();
+      }
+    }
+    public int hashFunction(K key){
+        int hc=key.hashCode();
+        int bi=Math.abs(hc)%buckets.length;
+        return bi;
+    }
+    public int findInBucket(int bi,K key){
+        int di=0;
+        for(HMNode node : buckets[bi]){
+            if(node.key.equals(key)){
+                return di;
+            }
+            di++;
+        }
+        return -1;
+    }
+    public void put(K key, V value) throws Exception {
+      // write your code here
+      int bi=hashFunction(key);
+      int di=findInBucket(bi,key);
+      if(di!=-1){
+          HMNode node=buckets[bi].get(di);
+          node.value=value;
+          
+      }else{
+          HMNode node=new HMNode(key,value);
+          buckets[bi].addLast(node);
+          size++;
+      }
+      double lambda= (size*1.0)/buckets.length;
+      if(lambda>2.0){
+          rehashing();
+      }
+    }
+    public void rehashing() throws Exception{
+        LinkedList<HMNode> []oldBuckets=buckets;
+        initbuckets(2*oldBuckets.length);
+        size=0;
+        for(LinkedList<HMNode> bucket : oldBuckets){
+          for(HMNode node : bucket){
+              put(node.key,node.value);
+          }
+      }
+    }
+    public V get(K key) throws Exception {
+      // write your code here
+      int bi=hashFunction(key);
+      int di=findInBucket(bi,key);
+      if(di!=-1){
+          HMNode node=buckets[bi].get(di);
+          return node.value;
+          
+      }else{
+          return null;
+      }
+    }
+
+    public boolean containsKey(K key) {
+      // write your code here
+      int bi=hashFunction(key);
+      int di=findInBucket(bi,key);
+      if(di!=-1){
+         return true;
+      }else{
+          return false;
+      }
+    }
+
+    public V remove(K key) throws Exception {
+      // write your code here
+      int bi=hashFunction(key);
+      int di=findInBucket(bi,key);
+      if(di!=-1){
+          HMNode node=buckets[bi].remove(di);
+          size--;
+          return node.value;
+          
+      }else{
+          return null;
+      }
+    }
+
+    public ArrayList<K> keyset() throws Exception {
+      // write your code here
+      ArrayList<K> keys=new ArrayList<>();
+      for(LinkedList<HMNode> bucket : buckets){
+          for(HMNode node : bucket){
+              keys.add(node.key);
+          }
+      }
+      return keys;
+    }
+
+    public int size() {
+      // write your code here
+      return size;
+    }
+
+    public void display() {
+      System.out.println("Display Begins");
+      for (int bi = 0; bi < buckets.length; bi++) {
+        System.out.print("Bucket" + bi + " ");
+        for (HMNode node : buckets[bi]) {
+          System.out.print(node.key + "@" + node.value + " ");
+        }
+        System.out.println(".");
+      }
+      System.out.println("Display Ends");
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    HashMap<String, Integer> map = new HashMap();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("put")) {
+        String[] parts = str.split(" ");
+        String key = parts[1];
+        Integer val = Integer.parseInt(parts[2]);
+        map.put(key, val);
+      } else if (str.startsWith("get")) {
+        String[] parts = str.split(" ");
+        String key = parts[1];
+        System.out.println(map.get(key));
+      } else if (str.startsWith("containsKey")) {
+        String[] parts = str.split(" ");
+        String key = parts[1];
+        System.out.println(map.containsKey(key));
+      } else if (str.startsWith("remove")) {
+        String[] parts = str.split(" ");
+        String key = parts[1];
+        System.out.println(map.remove(key));
+      } else if (str.startsWith("size")) {
+        System.out.println(map.size());
+      } else if (str.startsWith("keyset")) {
+        System.out.println(map.keyset());
+      } else if (str.startsWith("display")) {
+        map.display();
+      }
+      str = br.readLine();
+    }
+  }
+}
